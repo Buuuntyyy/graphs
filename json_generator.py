f0c7da23a0338f10df0b10701af8a62189fcb912
Bunnntyyy <104865101+Buuuntyyy@users.noreply.github.com>
2025-06-10 11:11:40 +0200

    School and students names are real names and in different json

--- json_generator.py
+++ json_generator.py
@@ -1,14 +1,22 @@
 import json
 import random
+from faker import Faker
 
 def json_generator():
+    fake_student = Faker('en_US')
+    fake_schools = Faker()
+
+    prefixes = ['Universite', 'Institut', 'Ecole', 'Academie', 'College', 'Universite Technologique']
+    locations = [fake_schools.city() for _ in range(10)]
+    fields = ['des Sciences', 'de Technologie', 'd\'Ingenierie', 'de Medecine', 'des Arts', 'du Droit']
+
     # Paramètres de génération
     num_schools = int(input("How many schools are available for students ? (Choose an integer)\n"))
     num_students = int(input("How many Student are serenading the schools ?\n"))
 
     # Générer les noms
-    school_names = [f"School {i+1}" for i in range(num_schools)]
-    student_names = [f"Student {i+1}" for i in range(num_students)]
+    school_names = [f"{random.choice(prefixes)} {random.choice(locations)} {random.choice(fields)}" for i in range(num_schools)]
+    student_names = [f"{fake_student.first_name()}" for i in range(num_students)]
 
     # Générer les écoles avec des capacités et des préférences aléatoires
     schools = []
@@ -31,33 +39,22 @@ def json_generator():
         })
 
     # Regrouper les données
-    data = {
+    data1 = {
         "schools": schools,
+    }
+
+    data2 = {
         "students": students
     }
 
     # Sauvegarder dans un fichier JSON
-    with open("large_matching_data.json", "w") as f:
-        json.dump(data, f, indent=4)
+    with open("schools.json", "w") as f:
+        json.dump(data1, f, indent=4)
+
+    with open("students.json", "w") as f:
+        json.dump(data2, f, indent=4)
 
     print(f"Fichier JSON généré avec succès avec {num_schools} écoles et {num_students} étudiants.")
 
-    school_table = {
-        "schools": [
-            {
-                "name": school["name"],
-                "preferences": school["preferences"]
-            }
-            for school in data["schools"]
-        ]
-    }
 
-    students_table = {
-        "students": [
-            {
-                "name": students["name"],
-                "preferences": students["preferences"]
-            }
-            for students in data["schools"]
-        ]
-    }
+json_generator()
