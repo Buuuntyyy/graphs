e6618a439f0eca0ac1e2fe22557ebdd606d57590
Bunnntyyy <104865101+Buuuntyyy@users.noreply.github.com>
2025-06-13 10:53:47 +0200

    modif

--- json_generator.py
+++ json_generator.py
@@ -13,7 +13,7 @@ def json_generator():
     # Paramètres de génération
     num_schools = int(input("How many schools are available for students ? (Choose an integer)\n"))
     num_students = int(input("How many Student are serenading the schools ?\n"))
-    available_capacity = int(input("Choose the maximum capacity of schools ? (Minimum : 5)")) ; assert available_capacity > 5, "Max capacity must greater than 5"
+    available_capacity = int(input("Choose the maximum capacity of schools ? (Minimum : 5)")) ; assert available_capacity > 1, "Max capacity must greater than 5"
     # Générer les noms
     school_names = [f"{random.choice(prefixes)} {random.choice(locations)} {random.choice(fields)}" for i in range(num_schools)]
     student_names = [f"{fake_student.first_name()}" for i in range(num_students)]
@@ -22,7 +22,7 @@ def json_generator():
     schools = []
     total_capacity = 0
     for name in school_names:
-        capacity = random.randint(5, available_capacity)  # ou une valeur fixe si tu préfères
+        capacity = random.randint(1, available_capacity)  # ou une valeur fixe si tu préfères
         preferences = random.sample(student_names, len(student_names))
         total_capacity = capacity + total_capacity
         schools.append({
